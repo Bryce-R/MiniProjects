5308b16f62a4f3b8f6dbf998fe33bb3b2dc589fd
Pengkai Ru <pru@saicusa.com>
2017-09-18 00:10:52 -0700

    use bicycle model to do hybrid A star

--- motion_planning/Astar_test.py
+++ motion_planning/Astar_test.py
@@ -2,9 +2,8 @@ import math
 import numpy as np
 import Queue
 import pylab as pl
+import Car
 
-class Car(object):
-    pass
 
 class Astar(object):
     def __init__(self, start, goal):
@@ -16,6 +15,10 @@ class Astar(object):
         self.f_cost = 0.1
         self.v = 0.5
         self.steering_change = 2.0
+        self.Car = Car.Car(2.66, 1.5, 1.0, start) 
+        self.Car.visualize_as_center = False
+        self.model = self.Car.integrate_car
+        self.visualize = self.Car.visualize
 
     def cont_2_dist(self, current_pos):
         current_cell = (np.floor(current_pos[0]/self.cellsize[0]), np.floor(current_pos[1]/self.cellsize[1]), np.floor(current_pos[2]/self.cellsize[2]) )
@@ -23,9 +26,9 @@ class Astar(object):
     
     def heuristic(self, current):
         # cost_h = (current[0] - self.goal[0])**2 + (current[1] - self.goal[1])**2 + (current[2] - self.goal[2])**2
-        w_x = 100.0
-        w_y = 1.0
-        w_theta = 1.0
+        w_x = 50.0
+        w_y = 10.0
+        w_theta = 10.0
         if current[0] < self.goal[0]:
             cost_h = np.sqrt( (current[0] - self.goal[0])**2*w_x**2 + (current[1] - self.goal[1])**2*w_y**2 ++ (current[2] - self.goal[2])**2*(w_theta)**2 )
         else:
@@ -33,7 +36,7 @@ class Astar(object):
         # cost_h = current[0] - self.goal[0] + np.sqrt( (current[1] - self.goal[1])**2*w_y**2 ++ (current[2] - self.goal[2])**2*(w_theta)**2)
         return cost_h
     
-    def model(self, current_pos, steering):
+    def euler_int_model(self, current_pos, steering):
         next_pos_x = current_pos[0]+np.cos(current_pos[2])*self.v
         next_pos_y = current_pos[1]+np.sin(current_pos[2])*self.v
         next_pos_heading = current_pos[2] + steering
@@ -41,8 +44,15 @@ class Astar(object):
         return next_pos
     
     def get_neighbors(self, current_pos):
-        neighbors = [ self.model(current_pos, 0.0), self.model(current_pos, self.steering_change/180.0*np.pi), self.model(current_pos, -self.steering_change/180.0*np.pi) ]
+        # neighbors = [ self.model(current_pos, 0.0), self.model(current_pos, self.steering_change/180.0*np.pi), self.model(current_pos, -self.steering_change/180.0*np.pi) ]
+        neighbors = [ self.model(current_pos, 0.0, 0.2), self.model(current_pos, 20./180.0*np.pi, 0.2), self.model(current_pos, -20./180.0*np.pi, 0.2) ]        
         return neighbors
+    
+    def close_to_goal(self, current_cell):
+        for i in xrange(3):
+            if abs(current_cell[i] - self.goal_cell[i])>1:
+                return False
+        return True
         
     def Astar_path_generation(self):
         close_set = {}
@@ -54,20 +64,20 @@ class Astar(object):
         open_q.put( ( total_cost[(self.start, self.start_cell)], self.start, self.start_cell ) )
         while len(open_set) > 0:
             current_node = open_q.get()
-            current_cell = current_node[2]
             current_pos = current_node[1]
-            # print current_pos
-            pl.plot(current_pos[0], current_pos[1], 'r+')
-            pl.pause(0.01)
-            if current_cell == self.goal_cell:
-                return self.reconstruct_path(current_cell, current_pos, ancestor)
-            
+            current_cell = current_node[2]
+
+            self.visualize(current_pos)
+            print current_cell, self.goal_cell
+            if self.close_to_goal(current_cell) :
+                return self.reconstruct_path(current_pos, current_cell, ancestor)
             current_moving_cost = moving_cost[ current_cell ]
             del open_set[current_cell]
             close_set[ current_cell] = current_pos
             for neighbor in self.get_neighbors(current_pos):
+                neighbor = tuple(neighbor)
                 neighbor_cell = self.cont_2_dist(neighbor)
-                # print neighbor, neighbor_cell
+                
                 neighbor_moving_cost = current_moving_cost + self.f_cost
                 if neighbor_cell not in close_set:
                     if neighbor_cell in open_set:
@@ -85,20 +95,19 @@ class Astar(object):
                         ancestor[(neighbor, neighbor_cell)] =   (current_pos, current_cell)    
         print 'Path not found!!!!!!!!! '
         return False 
-        
-        def reconstruct_path(self, current_pos, current_cell, ancestor):
-            print '------------Path found!'
-            total_path = [current_pos]
-            while ancestor[(current_pos, current_cell)] is not None:
-                current_pos, current_cell = ancestor[(current_pos, current_cell)]
-                total_path += [ current_pos ]
-    
+    def reconstruct_path(self, current_pos, current_cell, ancestor):
+        print '------------Path found!'
+        total_path = [current_pos]
+        while ancestor[(current_pos, current_cell)] is not None:
+            current_pos, current_cell = ancestor[(current_pos, current_cell)]
+            # print current_pos, current_cell
+            total_path += [ current_pos ]
     
 def main():
     start = (0.0, 0.0, np.pi/2.0)
-    goal =  (15.0, 15.0, 0.0)
+    goal =  (15.0, 15.0, np.pi/2.)
     planner = Astar(start, goal)
-    
+    # set up plotting 
     pl.figure(figsize=(8*1.1, 6*1.1))
     pl.plot(start[0], start[1], 'og', label = 'start')
     pl.plot(goal[0],  goal[1], '+g', label = 'end')
@@ -107,6 +116,9 @@ def main():
     pl.xlabel('x (meters)')
     pl.ylabel('y (meters)')
     pl.legend(fancybox=True, framealpha=0.5)
+    planner.visualize(start)
+    planner.visualize(goal)
+    
 
     path = planner.Astar_path_generation()
     print path
--- motion_planning/Car.py
+++ motion_planning/Car.py
@@ -4,11 +4,12 @@ import pylab as pl
 from scipy.integrate import odeint
 
 class Car(object):
-    def __init__(self, length, width, v, current_state= None):
+    def __init__(self, length, width, v, current_state):
         self._length = length
         self._width = width
         self._v = v
         self.current_state = current_state
+        self.visualize_as_center = True
     def kinematics(self, current_state, t, control):
         # current_state = [x, y, theta]
         dstate = [0.0, 0.0, 0.0]
@@ -17,7 +18,9 @@ class Car(object):
         dstate[2] = control
         return dstate
     def integrate_car(self, current_state, control, dt):
-        sol = odeint(self.kinematics, current_state, np.linspace(0, dt, 10), args = (control,) )
+        if current_state is None:
+            current_state = self.current_state
+        sol = odeint(self.kinematics, current_state, np.linspace(0, dt, 11), args = (control,) )
         next_state = [0.0, 0.0, 0.0]
         next_state[0], next_state[1], next_state[2] = sol[-1,0], sol[-1,1], sol[-1,2]
         self.current_state = next_state
@@ -26,30 +29,43 @@ class Car(object):
         # r.set_initial_value(current_state, 0.0)
         # r.integrate(r.t+dt)
         # assert (r.successful()), "integration failed."
-    def visulize(self):
+    def visualize(self, current_state = None, color = 'b-'):
+        if current_state is not None:
+            self.current_state = current_state
         assert self.current_state is not None
         car_pts = np.zeros( (2,5), dtype = np.double )
-        car_pts[:,0] = np.array([self.current_state[0]+self._length/2.0, self.current_state[1]+self._width/2.0])
-        car_pts[:,1] = np.array([self.current_state[0]+self._length/2.0, self.current_state[1]-self._width/2.0])
-        car_pts[:,2] = np.array([self.current_state[0]-self._length/2.0, self.current_state[1]-self._width/2.0], ndmin = 2)
-        car_pts[:,3] = [self.current_state[0]-self._length/2.0, self.current_state[1]+self._width/2.0]
-        car_pts[:,4] = [self.current_state[0]+self._length/2.0, self.current_state[1]+self._width/2.0]
-        transform = np.array( [ [np.cos(self.current_state[2]), np.sin(self.current_state[2])],[-np.sin(self.current_state[2]), np.cos(self.current_state[2])] ] )
-        car_pts = np.dot(transform, car_pts)       
-        pl.plot( car_pts[0,:], car_pts[1,:], 'b-' )
+        car_pts[:,0] = [ self._length/2.0,  self._width/2.0]
+        car_pts[:,1] = [ self._length/2.0, -self._width/2.0]
+        car_pts[:,2] = [-self._length/2.0, -self._width/2.0]
+        car_pts[:,3] = [-self._length/2.0,  self._width/2.0]
+        car_pts[:,4] = [ self._length/2.0,  self._width/2.0]
+        transform = np.array( [ [np.cos(self.current_state[2]), -np.sin(self.current_state[2])],[np.sin(self.current_state[2]), np.cos(self.current_state[2])] ] )
+        car_pts = np.dot(transform, car_pts)   
+        car_pts += np.array( [[self.current_state[0]], [self.current_state[1]] ] )
 
+        ax_car = pl.plot( car_pts[0,:], car_pts[1,:], color )
+        if self.visualize_as_center:
+            pl.xlim([self.current_state[0]-20., self.current_state[0]+20.])
+            pl.ylim([self.current_state[1]-20., self.current_state[1]+20.])
         pl.pause(0.01)
         
 if __name__ == '__main__':
     current_state = [0.0, 0.0, 0.0]
     print current_state
     myCar = Car(2.66, 1.5, 1.0, current_state) 
+    myCar.visualize_as_center = True
     pl.figure(figsize=(8*1.1, 6*1.1))
-    myCar.visulize()
+    pl.axis('equal')
+    pl.grid('on')
+    pl.xlabel('x (meters)')
+    pl.ylabel('y (meters)')
+    # pl.legend(fancybox=True, framealpha=0.5)
     
-    next_state = myCar.integrate_car(current_state, 0.1, 0.5)
-    myCar.visulize()
+    myCar.visualize()
+    for i in xrange(20):
+        next_state = myCar.integrate_car(None, 0.1, 0.5)
+        myCar.visualize()
+        print next_state
+    
     pl.show()
     
-    
-    print next_state
