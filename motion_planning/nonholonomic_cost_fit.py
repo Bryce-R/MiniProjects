bc5761bf2502ad7fa2149c6d1f2540075c6e1013
Pengkai Ru <pru@saicusa.com>
2017-09-20 00:09:54 -0700

    adding cost func fit file

---
+++ motion_planning/nonholonomic_cost_fit.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pylab as pl
+import Car
+import pickle
+import random
+import os
+import nonholonomic_cost_func
+
+
+def main():
+    
+    pl.figure(figsize=(8*1.1, 6*1.1))
+    pl.axis('equal')
+    pl.grid('on')
+    pl.xlabel('x (meters)')
+    pl.ylabel('y (meters)')
+    pl.legend(fancybox=True, framealpha=0.5)
+    pl.xlim([-20., 20.])
+    pl.ylim([-20., 20.])
+    
+    start = (0.0, 0.0, 0.0)
+    myCar = Car.Car(2.66, 1.5, -1.0, start) 
+    myCar.visualize_as_center = False
+    if os.path.isfile('cost_func.pickle') :
+        with open('cost_func.pickle', 'rb') as handle:
+            cost_func = pickle.load(handle)
+    for current_cell in cost_func:
+        current_pos = nonholonomic_cost_func.dist_2_cont(current_cell)
+        myCar.visualize(current_pos, 'c:')
+    print 'End.'
+
+
+    
+if __name__ == '__main__':
+    main()  
--- motion_planning/nonholonomic_cost_func.py
+++ motion_planning/nonholonomic_cost_func.py
@@ -3,14 +3,18 @@ import pylab as pl
 import Car
 import pickle
 import random
-import os.path
+import os
 
-def cont_2_dist(self, current_pos):
-    current_cell = (np.floor(current_pos[0]/self.cellsize[0]), np.floor(current_pos[1]/self.cellsize[1]), np.floor(current_pos[2]/self.cellsize[2]) )
+def cont_2_dist(current_pos):
+    current_cell = (np.floor(current_pos[0]/0.01), np.floor(current_pos[1]/0.01), np.floor(current_pos[2]/(0.5/180.*np.pi)) )
     return current_cell
 
+def dist_2_cont(current_cell):
+    current_pos = (current_cell[0]*0.01, current_cell[1]*0.01, current_cell[2]*(0.5/180.*np.pi) )
+    return current_pos
+
 def main():
-    start = (0.0, 0.0, np.pi/2.0)
+    start = (0.0, 0.0, 0.0)
     myCar = Car.Car(2.66, 1.5, -1.0, start) 
     myCar.visualize_as_center = False
     control_actions = [ i/180.*np.pi for i in xrange(1,11) ]
@@ -23,8 +27,12 @@ def main():
         cost_func = {start: 0.0}
         for action in control_actions:
             next_state = myCar.integrate_car(start, action, dt)
-            cost_func[ tuple(next_state) ] = action*2.+ 0.01
+            print next_state
+            next_cell = cont_2_dist(next_state)
+            cost_func[ next_cell ] = action*2.+ 0.01
         with open('cost_func.pickle', 'wb') as handle:
+            print 'dump:'
+            print cost_func
             pickle.dump(cost_func, handle, protocol=pickle.HIGHEST_PROTOCOL)
         
 
@@ -33,15 +41,21 @@ def main():
         if os.path.isfile('cost_func.pickle') :
             with open('cost_func.pickle', 'rb') as handle:
                 cost_func = pickle.load(handle)
-        current_state = random.choice(cost_func.keys())
+                os.remove('cost_func.pickle')
+                print 'load', ct
+                # print cost_func
+        current_cell = random.choice(cost_func.keys())
+        current_pos = dist_2_cont(current_cell)
         for action in control_actions:
-            next_state = tuple( myCar.integrate_car(current_state, action, dt) )
-            if next_state in cost_func:
-                cost_func[ next_state ] = min( cost_func[current_state]+ action*2.+ 0.01, cost_func[ next_state ])
+            next_state = tuple( myCar.integrate_car(current_pos, action, dt) )
+            next_cell = cont_2_dist(next_state)
+            if next_cell in cost_func:
+                cost_func[ next_cell ] = min( cost_func[current_cell]+ action*2.+ 0.01, cost_func[ next_cell ])
             else:
-                cost_func[ next_state ] = cost_func[current_state]+ action*2.+ 0.01
+                cost_func[ next_cell ] = cost_func[current_cell]+ action*2.+ 0.01
         ct += 1
         if ct%1000 == 0:
+            print ('length of cost_func: {} .'.format( len(cost_func) ))
             ans = raw_input('Keep calculating or not?  (y/n)')
             if ans == 'n':
                 with open('cost_func.pickle', 'wb') as handle:
@@ -50,4 +64,5 @@ def main():
     
     
 if __name__ == '__main__':
+
     main()  
