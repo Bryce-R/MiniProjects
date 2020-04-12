ad13767740ae92de2027365e4e9d5a0dbf0ddb20
Pengkai Ru <pengkai@Pengkais-MacBook-Pro.local>
2020-04-12 15:06:15 -0700

    update plot

--- trading/vix_vs_spy.py
+++ trading/vix_vs_spy.py
@@ -12,7 +12,7 @@ quandl.ApiConfig.api_key = "Dne4SGor1UQsBqsyGP3X"
 # plt.style.use('Solarize_Light2')
 
 start_date = "2020-03-01"
-end_date = "2020-04-11"
+end_date = "2020-04-10"
 
 # plt.subplot(211)
 # https://docs.quandl.com/docs/python-time-series
@@ -20,7 +20,7 @@ end_date = "2020-04-11"
 vix = quandl.get("CHRIS/CBOE_VX1.4", start_date=start_date,
                  end_date=end_date)
 # vix.plot()
-# print vix.values
+# print vix.axes
 vixVal = vix.values
 # Need subscription or premium API
 # spy = quandl.get("EOD/SPY.4", start_date=start_date,
@@ -34,11 +34,12 @@ vixVal = vix.values
 
 
 spyPrice = yf.download('SPY', start_date, end_date)
-# print spyPrice
+print spyPrice.axes[0]
 # plt.subplot(212)
 # spyPrice.Close.plot()
-# plt.grid("on")
+# plt.grid()
 # plt.legend()
+
 
 # plt.show()
 spyClose = spyPrice.Close.to_numpy()
@@ -52,14 +53,19 @@ for i in range(1, spyClose.shape[0]):
     spyChangeHigh[i] = (spyHigh[i]-spyClose[i-1])/spyClose[i-1]*100.0
     spyChangeLow[i] = (spyLow[i]-spyClose[i-1])/spyClose[i-1]*100.0
 
+
 # print spyNp
 sqrtDays = np.sqrt(252)
-plt.figure()
-plt.plot(spyChangePercent, 'g--+', label="spyChange")
-plt.plot(spyChangeHigh, "b-", label="spyChangeHigh")
-plt.plot(spyChangeLow, "r--+", label="spyChangeLow")
-plt.plot(vixVal/sqrtDays, "--", label="vix-IV")
-plt.plot(-vixVal/sqrtDays, "--", label="vix-IV")
+plt.figure(figsize=[12, 8])
+plt.plot(spyPrice.axes[0], spyChangePercent, 'k', label="spyChange")
+plt.plot(spyPrice.axes[0], spyChangeHigh, "go", label="spyChangeHigh")
+plt.plot(spyPrice.axes[0], spyChangeLow, "ro", label="spyChangeLow")
+for i in range(spyClose.shape[0]):
+    plt.plot([spyPrice.axes[0][i], spyPrice.axes[0][i], spyPrice.axes[0][i]], [
+             spyChangeHigh[i], spyChangePercent[i], spyChangeLow[i]], "b-")
+plt.plot(vix.axes[0], vixVal/sqrtDays, "--", label="vix-IV")
+plt.plot(vix.axes[0], -vixVal/sqrtDays, "--", label="vix-IV")
+plt.xticks(rotation=0)
 plt.grid()
 plt.legend(fancybox=True, framealpha=0.3, loc="best")
 plt.show()
