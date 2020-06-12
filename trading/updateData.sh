1207bf77088c7399efe98ce2366236d11ddb550e
Bryce R <pengkai.ru@gmail.com>
2020-06-11 22:56:21 -0700

    curl gvx

--- trading/updateData.sh
+++ trading/updateData.sh
@@ -3,3 +3,5 @@
 curl http://www.cboe.com/publish/scheduledtask/mktdata/datahouse/rvxdailyprices.csv --output data/rvx.csv
 
 curl http://www.cboe.com/publish/scheduledtask/mktdata/datahouse/vixcurrent.csv --output data/vix.csv
+
+curl http://www.cboe.com/publish/scheduledtask/mktdata/datahouse/gvzhistory.csv --output data/gvx.csv
