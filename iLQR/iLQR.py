1c84c2a98271863aaaa9ea9d8896299052c35251
Bryce R <pengkai.ru@gmail.com>
2023-01-01 12:53:51 -0800

    update iLQR

--- iLQR/iLQR.py
+++ iLQR/iLQR.py
@@ -20,15 +20,27 @@ def integrate(x, y, theta, v, curvature):
     return (x_next, y_next, theta_next)
 
 
+def f_x(x, y, theta, v, curvature):
+    f_x = np.zeros([numState, numState], dtype=np.double)
+    f_x[0, 0] = 1  # dx/dx
+    f_x[0, 2] = -v*np.sin(theta)  # dx/d  theta
+    f_x[1, 1] = 1
+    f_x[1, 2] = v*np.cos(theta)  # dy/d(theta)
+    f_x[2, 2] = 1
+    return f_x
+
+
 class iLQR(object):
-    def __init__(self, numInput, numState, horizon, Q, R):
+    def __init__(self, numInput, numState, horizon, Q, R, Qf):
         self._numInput = numInput
         self._numState = numState
         self._horizon = horizon
         self._Q = Q
         self._R = R
+        self._Qf = Qf
         assert(_Q.shape == (_numState, _numState))
-        assert(_Q.shape == (_numInput, _numInput))
+        assert(_Qf.shape == (_numState, _numState))
+        assert(_R.shape == (_numInput, _numInput))
 
     def solve(reference, control):
         assert(reference.shape == (_numState, _horizon))
@@ -49,7 +61,31 @@ class iLQR(object):
             self.updated_output[2, i] = theta_next
 
     def backwardPass():
-        pass
+        # Eqs (5a), (5b) and (5c).
+        # Q_x = l_x + f_x.T.dot(V_x)
+        # Q_u = l_u + f_u.T.dot(V_x)
+        # Q_xx = l_xx + f_x.T.dot(V_xx).dot(f_x)
+
+        # Eqs (11b) and (11c).
+        # reg = self._mu * np.eye(self.dynamics.state_size)
+        # Q_ux = l_ux + f_u.T.dot(V_xx + reg).dot(f_x)
+        # Q_uu = l_uu + f_u.T.dot(V_xx + reg).dot(f_u)
+        # k = -(Q_uu)^-1 * Q_u
+        # K = -(Q_uu)^-1 * Q_ux
+        # u(i)_new =  u(i) + k(i) + K(i)*[x(i) - x_ref(i)]
+
+        # Vf = xf^T * Qf * xf
+        # final state
+        Vxx = _Qf
+        Vx = self.updated_output[:, -1]*_Qf
+
+        for i in range(self.updated_output.shape(1), -1):
+            l_x = self.updated_output[:, i]*_Q  # l_x = x^T * Q * x
+            f_x = f_x(self.updated_output[0, i], self.updated_output[1, i],
+                      self.updated_output[2, i], self._control[0, i-1], self._control[1, i-1])
+            Q_x = l_x + f_x.transpose()*Vx
+            l_u = self._control[0, i-1]*_R
+            # Q_u = l_u + f_u.transpose()*Vx
 
 
 if __name__ == '__main__':
